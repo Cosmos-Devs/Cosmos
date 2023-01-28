01eb4dcafb56abe02ce913a32e5c25ba2bfbbb33
Niels Mentink <niels@ment.ink>
2023-01-28 12:56:04 +0100

    ♻️ Refactored core package

--- examples/basic/src/routes/+page.svelte
+++ examples/basic/src/routes/+page.svelte
@@ -1,8 +1,6 @@
 <script lang="ts">
 	import { Hello } from '@cosmos-devs/core';
-
-	console.log(Hello);
 </script>
 
-<h1>Welcome to SvelteKit</h1>
+<h1>{Hello}</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
