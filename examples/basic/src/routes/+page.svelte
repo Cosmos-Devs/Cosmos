acb3d3f130aaee1805f629a16d21eab49a5d5dff
Niels Mentink <niels@ment.ink>
2023-01-28 12:45:59 +0100

    ✨ Added core package

--- examples/basic/src/routes/+page.svelte
+++ examples/basic/src/routes/+page.svelte
@@ -1,2 +1,8 @@
+<script lang="ts">
+	import { Hello } from '@cosmos-devs/core';
+
+	console.log(Hello);
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
