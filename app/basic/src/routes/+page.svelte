79ff95b17a208065a0f92d5eb645e72b8c8dd47c
Niels Mentink <niels@ment.ink>
2023-01-31 22:43:56 +0100

    🚧 more WIP

--- app/basic/src/routes/+page.svelte
+++ app/basic/src/routes/+page.svelte
@@ -2,6 +2,6 @@
 	import { Test } from '@cosmos-devs/core';
 </script>
 
-<Test />
+<Test name="Test" />
 
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
---
+++ packages/core/svelte.config.js
@@ -0,0 +1,9 @@
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  package: {
+    source: "src",
+    dir: "dist",
+  },
+};
+
+export default config;
