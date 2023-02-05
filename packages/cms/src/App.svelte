c9cd4bea13d0ff4d9bc2caad498fc43ab50a4e93
Jochem van der Valk <jochemvandervalk@gmail.com>
2023-02-05 16:26:45 +0100

    loading screen

--- packages/cms/src/App.svelte
+++ packages/cms/src/App.svelte
@@ -1,16 +1,17 @@
 <script lang="ts">
-  import type { AdminConfig } from "./types";
-  import { page } from "$app/stores";
-  import { onMount, setContext } from "svelte";
-  import Editor from "./components/screens/Editor.svelte";
-  import "./app.css";
-  import Controller from "./lib/controller";
-  import { writable } from "svelte/store";
+  import './app.css';
+  import type { AdminConfig } from './types';
+  import { page } from '$app/stores';
+  import { writable } from 'svelte/store';
+  import { onMount, setContext } from 'svelte';
+  import Controller from './lib/controller';
+  import Editor from './components/screens/Editor.svelte';
+  import Loading from './components/screens/Loading.svelte';
 
   export let config: AdminConfig;
 
   const controller = writable<Controller | null>(null);
-  setContext("controller", controller);
+  setContext('controller', controller);
 
   let loading = true;
   onMount(() => {
@@ -19,13 +20,13 @@
   });
 
   $: path = $page.url.hash;
-  $: type = path.split("/")?.[1];
+  $: type = path.split('/')?.[1];
 </script>
 
 <main>
-  {#if loading}
-    <h1>Loading...</h1>
-  {:else if path.startsWith("#edit")}
+  {#if true}
+    <Loading />
+  {:else if path.startsWith('#edit')}
     <Editor template={config.register.templates[type]} />
   {:else}
     <a href="#edit/blog/test">Edit blog page</a>
---
+++ packages/cms/src/components/screens/Loading.svelte
@@ -0,0 +1,7 @@
+<script>
+  import logo from '../../assets/cosmos-logo.svg';
+</script>
+
+<figure class="w-full h-screen-s grid place-items-center">
+  <img src={logo} alt="Cosmos" class="w-28" />
+</figure>
