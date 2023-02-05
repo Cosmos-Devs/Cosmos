d2005dcab26e2a734695079faff542f5f63f54ab
Jochem van der Valk <jochemvandervalk@gmail.com>
2023-02-05 17:49:27 +0100

    page structure

--- packages/cms/src/components/screens/SignIn.svelte
+++ packages/cms/src/components/screens/SignIn.svelte
@@ -1,1 +1,21 @@
-<h1>Sign in</h1>
+<script>
+  import picture from '../../assets/pictures/nasa-img.jpg';
+</script>
+
+<div class="grid grid-cols-[min(100%,640px)_1fr]">
+  <div>
+    <h1>Sign in</h1>
+    <form>
+      <input type="email" placeholder="Email" />
+      <input type="password" placeholder="Password" />
+    </form>
+    <button>Sign in</button>
+  </div>
+
+  <img
+    class="sm:hidden justify-self-stretch items-self-stretch"
+    src={picture}
+    loading="lazy"
+    alt=""
+  />
+</div>
